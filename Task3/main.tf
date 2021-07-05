47170dc186ceb52b579decc2e73c84c03d96ca97
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2021-07-05 22:55:35 +0530

    Update main.tf

--- Task3/main.tf
+++ Task3/main.tf
@@ -1,54 +1,66 @@
 provider "aws" {
-
-  region  = "ap-south-1"
-  shared_credentials_file = "/Users/mansi/.aws/credentials"
+  region = "ap-south-1"
+  shared_credentials_file = "C:/Users/mansi/.aws/credentials"
   profile = "default"
-  
 }
 
-resource "aws_instance" "wordpress" {
-  ami           = "ami-011c99152163a87ae"
-  instance_type = "t2.micro"
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  name                   = var.dbname
+  username               = var.username
+  password               = var.password
+  parameter_group_name   = "default.mysql5.7"
   security_groups = ["web1-security-group"]
-  
+  db_subnet_group_name   = aws_db_subnet_group.mysql.name
+  skip_final_snapshot    = true
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-011c99152163a87ae"
+  instance_type = "t2.micro"
+  depends_on = [
+    aws_db_instance.mysql,
+  ]
+
   key_name = "devops"
-  tags = { 
-    Name = "wordpress"
-  }
-}
+  security_groups = ["web1-security-group"]
+  associate_public_ip_address = true
 
+  user_data = file("script.sh")
 
-
-resource "aws_db_instance" "default" {
-  identifier           = "wordpress1"
-  allocated_storage    = 20
-  
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "wordpress"
-  username             = "root"
-  password             = "hey123!!"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-} 
-resource "null_resource" "re2" {
-  
-  
-  
-connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("C:/Users/mansi/Downloads/devops.pem")
-    host = aws_instance.wordpress.public_ip
+  tags = {
+    Name = "EC2 Instance"
   }
 
-provisioner "remote-exec" {
-     inline = [
-      "sudo yum -y install httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd"
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("C:/Users/mansi/Downloads/devops.pem")
+      host = aws_instance.ec2.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh",
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("C:/Users/mansi/Downloads/devops.pem")
+      host = aws_instance.ec2.public_ip
+    }
+  }
+  timeouts {
+    create = "20m"
   }
 }
-
