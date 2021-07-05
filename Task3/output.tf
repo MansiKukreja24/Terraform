77a571725d9dd45092d9eaf9c1b0fdb06c736878
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2021-07-05 22:56:32 +0530

    Create output.tf

---
+++ Task3/output.tf
@@ -0,0 +1,15 @@
+output "Login" {
+  value = "ssh -i devops ec2-user@${aws_instance.ec2.public_ip}"
+}
+
+output "azs" {
+  value = data.aws_availability_zones.azs.*.names
+}
+
+output "db_access_from_ec2" {
+  value = "mysql -h ${aws_db_instance.mysql.address} -P ${aws_db_instance.mysql.port} -u ${var.username} -p${var.password}"
+}
+
+output "access" {
+  value = "http://${aws_instance.ec2.public_ip}/index.php"
+}
